24f539787550cf10d8a635882578013eadb23d99
theCR7 <44245637+theCR7@users.noreply.github.com>
2018-10-18 15:05:55 +0530

    Update install.sh

--- Ubuntu-18.04-Server-Installation-CMDS/install.sh
+++ Ubuntu-18.04-Server-Installation-CMDS/install.sh
@@ -8,30 +8,30 @@ wget http://bit.ly/restartxanc -O restartx
 wget http://bit.ly/setupacn
 chmod 0777 start
 chmod 0777 new
-chmod 0777 installtmx
+chmod 0777 installacn
 chmod 0777 restart
-chmod 0777 setuptmx
+chmod 0777 setupacn
 chmod 0777 restartx
 chmod 0777 stop
 chmod +x new
 chmod +x start
 chmod +x restart
-chmod +x installtmx
-chmod +x setuptmx
+chmod +x installacn
+chmod +x setupacn
 chmod +x restartx
 chmod +x stop
 sudo cp start /usr/bin/
 sudo cp restart /usr/bin/
 sudo cp restartx /usr/bin/
-sudo cp installtmx /usr/bin/
+sudo cp installacn /usr/bin/
 sudo cp stop /usr/bin/
 sudo cp new /usr/bin/
 sudo cp setuptmx /usr/bin/
 rm -f stop
 rm -f start
-rm -f installtmx
+rm -f installacn
 rm -f new
-rm -f setuptmx
+rm -f setupacn
 rm -f restart
 rm -f restartx
 echo "All Set Boi! Thanks for using M.A.T.R.I.X Group CREATION !"
